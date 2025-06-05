4c4782737688f8050abab33b1d3e43ac0e6b4b34
kako1996 <oto20221228@gmail.com>
2025-06-05 09:58:50 +0900

    spring_kadai_formをアップデート

---
+++ spring_kadai_form/src/main/java/com/example/springkadaiform/controller/ContactFormController.java
@@ -0,0 +1,50 @@
+package com.example.springkadaiform.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.example.springkadaiform.form.ContactForm;
+
+import jakarta.validation.Valid;
+
+@Controller
+public class ContactFormController {
+	
+	//GETリクエストの処理
+	@GetMapping("/contactForm")
+	public String contactForm(Model model) {
+		// すでにインスタンスが存在する場合は行わない
+        if (!model.containsAttribute("contactForm")) {
+            // ビューにフォームクラスのインスタンスを渡す
+            model.addAttribute("contactForm", new ContactForm());
+        }
+
+        return "contactFormView";
+	}
+	
+	//POSTリクエストの処理
+	@PostMapping("/confirm")
+	public String confirm(
+			@Valid ContactForm form, 
+			BindingResult result,
+			RedirectAttributes redirectAttributes) {
+		
+		// バリデーションエラーがあったら終了
+        if (result.hasErrors()) {
+            // フォームクラスをビューに受け渡す
+            redirectAttributes.addFlashAttribute("contactForm", form);
+            // バリデーション結果をビューに受け渡す
+            redirectAttributes.addFlashAttribute(BindingResult.MODEL_KEY_PREFIX + "contactForm", result);
+            
+            return "redirect:/contactForm";
+        }
+            
+        redirectAttributes.addFlashAttribute("contactForm", form);
+        return "confirmView";
+	}
+
+}
---
+++ spring_kadai_form/src/main/java/com/example/springkadaiform/form/ContactForm.java
@@ -0,0 +1,20 @@
+package com.example.springkadaiform.form;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class ContactForm {
+	//お名前
+	@NotBlank(message = "お名前を入力してください。")
+	private String name;
+	
+	//メールアドレス
+	@NotBlank(message = "メールアドレスの入力形式が正しくありません。")
+	private String email;
+	
+	//お問い合わせ内容
+	@NotBlank(message = "お問い合わせ内容を入力してください。")
+	private String message;
+
+}
